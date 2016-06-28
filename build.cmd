cae3d4340c1f5858d39755137f0a12136643e001
Ken Dale <ken@kendaleiv.com>
2016-06-28 11:14:02 -0400

    Change bundle exec gem install to bundle install

--- build.cmd
+++ build.cmd
@@ -12,7 +12,7 @@ if %ERRORLEVEL% neq 0 goto BuildFail
 call gem install bundler
 if %ERRORLEVEL% neq 0 goto BuildFail
 
-call bundle exec gem install
+call bundle install
 if %ERRORLEVEL% neq 0 goto BuildFail
 
 call jekyll build
