f8e2a9b7c88e3992268a8443518c2efcd3844587
Kevin Hougasian <kevin.hougasian@ritterim.com>
2016-11-07 14:55:32 -0500

    - change ttr append to :after and media query - add close &times; for sidebar close - increase sidebar articles to 20, expose more content

--- js/main.js
+++ js/main.js
@@ -3,18 +3,21 @@
   $('.ui.sidebar')
     .sidebar('attach events', '.icon.newspaper, .close');
 
-  $('.ui.sidebar')
-    .visibility({
-      once: false,
-      onPassing: function() {
-        $('.pusher article')
-          // .dimmer('blur');
-      },
-      onPassing: function() {
-        $('.pusher article')
-          // .dimmer('blur');
-      }
-  });
+  // don't see a way to handle this within semantic ui;
+  // could be wrong?
+  $('a .newspaper')
+    .on('click', function(e) {
+      e.preventDefault();
+  })
+
+  // add home menu item
+  $('.menu')
+    .hover(function() {
+      $(this)
+        .find('.home')
+        .toggleClass('visible');
+    });
+
 
   // (cc) image credit links will always vary in width.
   var $photoCredit = $('.photo-credit'),
@@ -33,29 +36,6 @@
       }, function() {
         $(this)
           .css( 'right', 'calc(-'+creditWidth+'px + 0.3em)');
-  });
-
-  $('*[data-freshness-datetime]').each(function(i, el) {
-    var $el = $(el);
-    var daysOld = parseInt((Date.now() - Date.parse($el.data('freshness-datetime'))) / 1000 / 60 / 60 / 24);
-
-    $el.find('.freshness-icon-js').each(function(i , iconEl) {
-      var $iconEl = $(iconEl);
-
-      if (daysOld <= 30) {
-        $iconEl.addClass('new');
-      } else if (daysOld >= 31 && daysOld <= 190) {
-        $iconEl.addClass('old');
-      } else if (daysOld >= 191 && daysOld <= 364) {
-        $iconEl.addClass('stale');
-      } else if (daysOld >= 365) {
-        $iconEl.addClass('rubbish');
-      }
-    });
-
-    $el.find('.freshness-days-old-js').each(function(i, daysOldEl) {
-      $(daysOldEl).text(daysOld);
-    });
   });
 
   // fix menu when passed
@@ -84,6 +64,29 @@
       type: 'image',
       transition: 'vertical flip in',
       duration: 500
+  });
+
+  $('*[data-freshness-datetime]').each(function(i, el) {
+    var $el = $(el);
+    var daysOld = parseInt((Date.now() - Date.parse($el.data('freshness-datetime'))) / 1000 / 60 / 60 / 24);
+
+    $el.find('.freshness-icon-js').each(function(i , iconEl) {
+      var $iconEl = $(iconEl);
+
+      if (daysOld <= 30) {
+        $iconEl.addClass('new');
+      } else if (daysOld >= 31 && daysOld <= 190) {
+        $iconEl.addClass('old');
+      } else if (daysOld >= 191 && daysOld <= 364) {
+        $iconEl.addClass('stale');
+      } else if (daysOld >= 365) {
+        $iconEl.addClass('rubbish');
+      }
+    });
+
+    $el.find('.freshness-days-old-js').each(function(i, daysOldEl) {
+      $(daysOldEl).text(daysOld);
+    });
   });
 
   var $postMainColumnJs = $('#post-main-column-js');
