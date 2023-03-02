9ebb12a642920cada49320c49fd3f6bd3fbf5abe
Ryan Trimble <mrtrimble2@gmail.com>
2023-03-02 10:19:53 -0500

    bugfix: update rss feed to pull from content collection

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -1,9 +1,8 @@
 import rss from '@astrojs/rss';
 import { site } from '../data/config.json';
 
-const postImportResult = import.meta.glob('../pages/*.md', { eager: true });
+const postImportResult = import.meta.glob('../content/posts/*.md', { eager: true });
 const posts = Object.values(postImportResult)
-  .sort((a, b) => a.frontmatter.date.localeCompare(b.frontmatter.date))
   .reverse();
 
 const feed = {
@@ -11,7 +10,7 @@ const feed = {
   description: site.description,
   site: site.baseurl,
   items: posts.map((post) => ({
-    link: post.url,
+    link: post.frontmatter.slug,
     title: post.frontmatter.title,
     description: post.frontmatter.twitter_text,
     pubDate: new Date(post.frontmatter.date),
